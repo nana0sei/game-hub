182211a3f598e4f3f0bbcd342050603998a0f6f0
nana0sei <frimpongnanao@gmail.com>
2023-08-04 11:11:31 +0000

    built platform selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,11 @@
+import useData from "./useData";
+
+interface Platform {
+  name: string;
+  id: number;
+  slug: string;
+}
+
+const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+
+export default usePlatforms;
