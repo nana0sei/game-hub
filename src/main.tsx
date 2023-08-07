5912509025b972058541f4f103a81a63b979ad37
nana0sei <frimpongnanao@gmail.com>
2023-08-07 17:00:22 +0000

    search functionality

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
+import "./index.css";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
