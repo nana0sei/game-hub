03c22424241bc6acadbd91dd23db29fe546ccc2a
nana0sei <frimpongnanao@gmail.com>
2023-09-28 16:22:15 +0000

    fetch game details

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Game } from "./useGames";
+
+//a hook for fetching a single game
+
+const apiClient = new APIClient<Game>("/games");
+
+const useGame = (slug: string) =>
+  useQuery({
+    queryKey: ["games", slug],
+    queryFn: () => apiClient.get(slug),
+  });
+
+export default useGame;
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,5 +1,6 @@
 import useGenres from "./useGenres";
 
+// a hook for fetching all genres
 const useGenre = (id?: number) => {
   const { data: genres } = useGenres();
   return genres?.results.find((g) => g.id === id);
