2fa402989a8887b6b1c4acfc22acef1c27154c3c
nana0sei <frimpongnanao@gmail.com>
2023-09-30 12:04:51 +0000

    building game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,15 @@
+import APIClient from "../services/api-client";
+import { Trailer } from "../entities/Trailer";
+import { useQuery } from "@tanstack/react-query";
+
+// hook for fetching game trailers
+const useTrailers = (gameId: number) => {
+  const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useTrailers;
